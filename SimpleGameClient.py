7a3ce970579f16d4d3c46ce065b729369644b988
jsantore <jsantore@bridgew.edu>
2019-12-04 13:45:20 -0500

    end of Wed Dec 4

--- SimpleGameClient.py
+++ SimpleGameClient.py
@@ -9,19 +9,33 @@ class GameClient(arcade.Window):
         super().__init__(1000, 1000, "Cliet")
         self.message = ""
         self.server_addr = addr
+        self.keys = ""
+        self.player = arcade.Sprite("archer.png", center_x= 550, center_y= 300)
 
     def on_update(self, delta_time: float):
         pass
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text(f"Got message from serve: {self.message}", 200, 200, color=(30, 40, 200), font_size=20)
+        arcade.draw_text(f"Got message: {self.message}", 200, 200, color=(230, 40, 20), font_size=20)
+        self.player.draw()
 
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.LEFT:
+            self.keys = f"LEFT{self.keys}"
+        elif symbol  == arcade.key.RIGHT:
+            self.keys = f"RIGHT{self.keys}"
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.LEFT:
+            self.keys = self.keys.replace("LEFT", "")
+        elif symbol == arcade.key.RIGHT:
+            self.keys = self.keys.replace("RIGHT", "")
 
 def communicate_with_server(client: GameClient):
     client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     while True:
-        network_message = str.encode("Client Says Hi")
+        network_message = str.encode(client.keys)
         client_socket.sendto(network_message, (client.server_addr, server.SERVER_PORT))
         data_packet = client_socket.recvfrom(1024)
         data = data_packet[0]
--- server.py
+++ server.py
@@ -16,14 +16,20 @@ def find_server_address():
 
 def run_server():
     server_address = find_server_address()
+    player_pos = [500, 600]
     print(f"Server Listening on {server_address}  Port: {SERVER_PORT}")
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((server_address, SERVER_PORT))
     while True:
         data_packet = server_socket.recvfrom(1024)
         data = data_packet[0]
+        data = str(data, "UTF-8")
+        if data.find("LEFT")>0:
+            player_pos[0] -=1
+        elif data.find("RIGHT")>0:
+            player_pos[0]+= 1
         client_address = data_packet[1]
-        encoded_message = str.encode(f"Server Got your message: {data}")
+        encoded_message = str.encode(f"{player_pos[0]},{player_pos[1]}")
         server_socket.sendto(encoded_message, client_address)
 
 if __name__ == '__main__':
